252d6f251ea577f5f816e29ffff2beb673cf5602
aws02 user <nik@cbdweb.net>
2020-04-20 13:40:27 +1000

    Update Newsletter plugins

---
+++ update_newsletter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-amazon --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-archive --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-automated --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-comments --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-extensions --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-facebook --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-reports --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+/home/lamp/wordpress/wp-cli/wp-cli.phar plugin update newsletter-woocommerce --allow-root --path=/home/lamp/webroot/spelfabet/ --url=https://www.spelfabet.com.au
+
